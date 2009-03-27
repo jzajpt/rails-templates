d50f71980cc33454cf3e9bebd0a7eac7d5873174
Jiri Zajpt <jzajpt@blueberry.cz>
2009-03-27 13:06:45 +0100

    Updated restful-auth template, added conditional authenticated generation.

--- restful-auth.rb
+++ restful-auth.rb
@@ -2,22 +2,14 @@ load_template "http://github.com/jzajpt/rails-templates/raw/master/base.rb"
 
 ##
 # Install plugin
-plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git', :submodule => true
+plugin 'restful-authentication',
+  :git => 'git://github.com/technoweenie/restful-authentication.git',
+  :submodule => true
 
 ##
 # Generate authenticated model and controller
-generate "authenticated", "user session"
-
-##
-# Convert authenticated views to HAML. Unfortunately html2haml generator is kinda broken
-# and generated views needs to be fixed, so don't forget to fix them.
-inside('app/views/') do
-  run "html2haml sessions/new.html.erb sessions/new.html.haml"
-  run "rm sessions/new.html.erb"
-  run "html2haml users/new.html.erb users/new.html.haml"
-  run "rm users/new.html.erb"
-  run "html2haml users/_user_bar.html.erb users/_user_bar.html.haml"
-  run "rm users/_user_bar.html.erb"
+if yes? 'Generate authenticated (user model, sessions controller)?'
+  generate "authenticated", "user session"
 end
 
 git :add => '.'
